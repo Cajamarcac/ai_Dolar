62bc23dc9e1dfedd0d3eb8a48b333cb582f2b278
Cajamarcac <fabiocajamarcaoficial@gmail.com>
2024-06-12 02:27:21 -0500

    agrega app_flask.py

---
+++ app_Flask/app_flask.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    df = pd.read_csv('valora_news.csv')
+    news = df.to_dict('records')
+    return render_template('index.html', news=news)
+
+if __name__ == '__main__':
+    app.run(debug=True)
