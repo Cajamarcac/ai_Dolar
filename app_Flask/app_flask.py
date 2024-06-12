a7fce4ed63d392fa46a74922e8fe34b3e7298b9a
Cajamarcac <fabiocajamarcaoficial@gmail.com>
2024-06-12 03:17:56 -0500

    modificacion de app_flask funciona bien

--- app_Flask/app_flask.py
+++ app_Flask/app_flask.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    df = pd.read_csv('valora_news.csv')
-    news = df.to_dict('records')
+def home():
+    # Ruta relativa al archivo CSV
+    csv_file_path = os.path.join(os.path.dirname(__file__), '..', 'valora_news.csv')
+
+    try:
+        df = pd.read_csv(csv_file_path)
+    except FileNotFoundError:
+        return "Archivo CSV no encontrado.", 404
+
+    # Convertir DataFrame a lista de diccionarios
+    news = df.to_dict(orient='records')
+
     return render_template('index.html', news=news)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
